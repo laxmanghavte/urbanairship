708488dad155137ce59ef477e5c8d8582fa256d0
Laxman Ghavte <laxman.ghavte@tcs.com>
2018-02-22 13:59:43 +0530

    removed extra line

--- pusher.php
+++ pusher.php
@@ -9,7 +9,6 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
 /* new urban airship */
-
 UALog::setLogHandlers(array(new StreamHandler("php://stdout", Logger::INFO)));
 $airship = new Airship("7MmAu9yWTl-Hb_q7631J0w", "6gmS3e57RcuIOEoBrWP7kQ");
 
